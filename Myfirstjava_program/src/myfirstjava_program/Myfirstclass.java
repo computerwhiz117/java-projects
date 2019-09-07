f285c5a745015bda621013fbdbb9307ee9960ed3
computerwhiz117 <computerwhiz117@gmail.com>
2019-09-07 13:25:08 -0400

    Add files via upload

---
+++ Myfirstjava_program/src/myfirstjava_program/Myfirstclass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstjava_program;
+
+/**
+ *
+ * @author carli
+ */
+public class Myfirstclass {
+ 
+    public static void main(String[] test)
+    {
+        //String info = "I love America";
+        //int infosize = info.length();
+        //int numb1 = 13;
+        //int numb2 = 11.0;
+        //double result = (double)(numb1 / numb2);
+        //System.out.println('A' + 'A');
+        //int infosize = info.length;
+        //boolean val = true;
+        //long val3 = 100;
+        //int val4 = 32000;
+        //float val5 = 10.5f;
+        //double val6 = 10.11111111111111111; 
+        //char char1 = 'A';
+        //int num = 2;
+        //char char2 = '2';
+        //char char3 = 10;
+        //int num1 = 10;
+        //System.out.println(char1);
+        //System.out.println(char2);
+    }
+    
+}
