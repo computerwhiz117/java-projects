31a11c0eca2660e9aa75aeba162811576755d2e1
Carlos Sanchez <computerwhiz117@gmail.com>
2019-09-24 14:44:46 -0400

    Changes in code

--- Shapes/src/shapes/Shapes.java
+++ Shapes/src/shapes/Shapes.java
@@ -16,24 +16,71 @@ public class Shapes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int rows, columns;
-       Scanner shapes = new Scanner(System.in);
-       System.out.println("Select your shape");
-       String type = shapes.nextLine();
-       System.out.println("Select the number of rows");
-       rows = shapes.nextInt();
-       System.out.println("Select the number of columns");
-       columns = shapes.nextInt();
+        int rows, columns;
+        String type;
+        String choice;
+        Scanner shapes = new Scanner(System.in);
+        System.out.println("Select the number of rows");
+        rows = shapes.nextInt();
+        System.out.println("Select the number of columns");
+        columns = shapes.nextInt();
+        
        
-       System.out.println("You are printing a box");
-       
-     for (int r = 0; r < rows; r++)
-     {
-         for (int c = 0; c < columns; c++)
-         {
-             System.out.print("*");
-         }
-         System.out.println();
-     }
-
-
+        
+        do{
+            System.out.println("Please select the type of shape");
+            System.out.println("1. Box");
+            System.out.println("2. Circle");
+            System.out.println("3. X");
+            System.out.println("4. Xbox");
+            System.out.println("5. Other");
+            System.out.println("To exit the program type exit");
+            choice = shapes.next(); 
+            
+        switch(choice)
+        {
+            case "Box":
+                ShapesMethod(rows, columns);
+                System.out.println("You are printing a box");
+                break;
+              
+            case "Circle":
+                System.out.println("You are printing a circle");
+                break;
+              
+            case "X":
+                System.out.println("You are printing an X");
+                break;
+            case "Xbox":
+                System.out.println("You are printing a box with an X inside");
+                break;
+              
+            case "Other":
+                System.out.println("You are printing an other shape");
+                break;
+               
+            case "exit":
+                System.out.println("Goodbye");
+                break;
+            default:
+                System.out.println("Please select a shape");
+              
+        }
+        }while(!choice.equals("exit"));  
+          
+    }
+    
+    
+    
+    static void ShapesMethod(int rows, int columns) {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+      }
+    }
+           
+    }
